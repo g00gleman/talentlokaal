8445d6c042634671ed74fba6518729e828fa640e
Gino Hanemaaijer <g.hanemaaijer11@gmail.com>
2023-01-21 14:15:07 +0100

    login page and register page and index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,22 @@
-<x-guest-layout>
-    <div class="bg-talent-light-green ml-2 mr-2 rounded-xl flex justify-center items-center m-64 w-96">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body class="bg-talent-green">
+    <div class="font-sans text-gray-900 antialiased  flex justify-center">
+    <div class="bg-talent-white ml-2 mr-2 rounded-xl flex justify-center items-center m-64 w-96">
         <div>
             <div class="flex-1 mt-3 ml-5">
                 <?xml version="1.0" encoding="UTF-8"?><svg
                     id="Laag_1"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 559.14 263.07"
-                    class=" w-80 mb-28"
+                    class=" w-80 mb-12"
                 >
                     <defs>
                         <style>
@@ -72,58 +82,14 @@
                     </g>
                 </svg>
             </div>
-        <x-jet-validation-errors class="mb-4" />
-<div class="justify-center flex mb-6">
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <div>
-              
-                <x-jet-input id="name" class=" px-5 border-none block mt-1 w-64 rounded-full shadow-xl" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Naam" />
-            </div>
-
-            <div class="mt-4">
-           
-                <x-jet-input id="email" class="px-5 border-none block mt-1 w-64 rounded-full shadow-xl" type="email" name="email" :value="old('email')" required placeholder="Email" />
-            </div>
-
-            <div class="mt-4">
-               
-                <x-jet-input id="password" class="px-5 border-none block mt-1 w-64 rounded-full shadow-xl" type="password" name="password" required autocomplete="new-password" placeholder="Wachtwoord" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-input id="password_confirmation" class="px-5 border-none block mt-1 w-64 rounded-full shadow-xl" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Herhaal wachtwoord" />
-            </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-jet-label for="terms">
-                        <div class="flex items-center">
-                            <x-jet-checkbox name="terms" id="terms" required />
-
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
-                                ]) !!}
-                            </div>
-                        </div>
-                    </x-jet-label>
-                </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="none-underline text-sm text-talent-white hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
+            <div class="justify-center flex -mb-5">
+            <button class=" h-9 ml-4 bg-talent-orange w-64 m-5 rounded-full text-talent-white justify-center shadow-xl">Log in</button>
+        </div>
+        <div class="justify-center flex mb-16">
+            <button class=" h-9 ml-4 bg-talent-orange w-64 m-5 rounded-full text-talent-white justify-center shadow-xl"> Registreer</button>
             </div>
         </div>
-<div class="justify-center flex mb-11">
-                <x-jet-button class="ml-4 bg-talent-orange w-64 m-5 rounded-full text-talent-white justify-center shadow-xl">
-                    {{ __('Register') }}
-                </x-jet-button>
-            </div>
-        </div>
-        </form>
-</x-guest-layout>
+    </div>
+</div>
+</body>
+</html>
