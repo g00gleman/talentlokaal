a3a52ac2b59276f2e84866b79248647257da8049
DarkMrAnime <gyllian121@gmail.com>
2023-02-11 17:00:02 +0100

    Log out modal done and works

--- resources/views/components/navbar/index.blade.php
+++ resources/views/components/navbar/index.blade.php
@@ -136,10 +136,86 @@
                     <div></div>
                     <div>Support</div>
                 </a>
-                <a href="/dashboard/logout" class="nav-content-btn" id="logOut">
+
+                <button
+                    class="nav-content-btn"
+                    onclick="openModal(); closeMenu();"
+                >
                     <div></div>
                     <div>Log uit</div>
-                </a>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .modal {
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        top: 0;
+        display: none;
+    }
+
+    .modal-content {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .modal-content-overlay {
+        height: 100vh;
+        background-color: black;
+        opacity: 0.5;
+        z-index: 1;
+    }
+
+    .modal-content-box {
+        height: 25vh;
+        width: 80vw;
+        background-color: white;
+        z-index: 2;
+        position: absolute;
+        top: 32.5vh;
+        left: 10vw;
+        border-radius: 16px;
+    }
+
+    .box-top {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 12.5vh;
+        color: #1e796a;
+        font-weight: bold;
+    }
+    .box-content {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        height: 7.5vh;
+    }
+    .box-content-btn {
+        background-color: #ef840c;
+        padding-left: 40px;
+        padding-right: 40px;
+        padding-top: 3px;
+        padding-bottom: 3px;
+        border-radius: 100px;
+        color: white;
+    }
+</style>
+
+<div class="modal" id="modal">
+    <div class="modal-content">
+        <div class="modal-content-overlay" onclick="closeModal()"></div>
+        <div class="modal-content-box">
+            <div class="box-top">Weet u zeker dat u wilt uitloggen?</div>
+            <div class="box-content">
+                <a href="/dashboard/logout" class="box-content-btn"> Ja </a>
+                <button class="box-content-btn" onclick="closeModal()">
+                    Nee
+                </button>
             </div>
         </div>
     </div>
@@ -151,6 +227,13 @@
     }
     function closeMenu() {
         document.getElementById("menu").style.display = "none";
+    }
+
+    function openModal() {
+        document.getElementById("modal").style.display = "block";
+    }
+    function closeModal() {
+        document.getElementById("modal").style.display = "none";
     }
 
     function check() {
