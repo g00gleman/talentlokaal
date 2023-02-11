80bd5444c24e08fa1a9ab92826a62df532dffd22
DarkMrAnime <gyllian121@gmail.com>
2023-02-11 15:15:27 +0100

    New navbar, its optimized

--- resources/views/components/navbar/index.blade.php
+++ resources/views/components/navbar/index.blade.php
@@ -1,208 +1,145 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-        <script
-            src="https://kit.fontawesome.com/8e80fea8d7.js"
-            crossorigin="anonymous"
-        ></script>
-        <link rel="stylesheet" href="style.css" />
-        <style>
-            html,
-            body {
-                margin: 0px;
-                padding: 0px;
-            }
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="menuIcon"
+    onclick="openMenu()"
+>
+    <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+    />
+</svg>
 
-            .menuIcon {
-                width: 50px;
-                height: 50px;
-                padding: 10px;
-                background-color: #bcd7d2;
-                color: #1e796a;
-                border-radius: 100px;
-                top: 0;
-                right: 0;
-                position: absolute;
-                margin-right: 15px;
-                margin-top: 15px;
-            }
+<style>
+    .menuIcon {
+        width: 50px;
+        height: 50px;
+        padding: 10px;
+        background-color: #bcd7d2;
+        color: #1e796a;
+        border-radius: 100px;
+        top: 0;
+        right: 0;
+        position: absolute;
+        margin-right: 15px;
+        margin-top: 15px;
+    }
 
-            .flex {
-                display: flex;
-            }
+    .menu {
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        top: 0;
+        display: none;
+    }
 
-            .menu {
-                left: 0;
-                right: 0;
-                position: absolute;
-                display: none;
-                height: 100vh;
-                width: 100vw;
-                z-index: 999;
-            }
+    .menu-content {
+        display: flex;
+    }
 
-            .overlay {
-                height: 100vh;
-                width: 30vw;
-                background-color: black;
-                opacity: 0.25;
-            }
+    .menu-content-overlay {
+        height: 100vh;
+        width: 100vw;
+        background-color: black;
+        opacity: 0.5;
+        z-index: 2;
+    }
 
-            .navbar {
-                height: 100vh;
-                width: 70vw;
-                background-color: white;
-            }
-            .top {
-                height: 15vh;
-                width: 100%;
-                background-color: #bcd7d2;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                font-weight: bold;
-                color: white;
-                font-size: 32px;
-                /* border-top-left-radius: 16px; */
-                border-bottom-left-radius: 16px;
-            }
+    .menu-content-nav {
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 100vh;
+        width: 60vw;
+        background-color: white;
+        border-bottom-left-radius: 16px;
+        border-top-left-radius: 16px;
+        z-index: 3;
+    }
 
-            .content {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-            }
+    .nav-top {
+        height: 15vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #bcd7d2;
+        font-size: 48px;
+        font-weight: bold;
+        color: white;
+        border-bottom-left-radius: 16px;
+        border-top-left-radius: 16px;
+        margin-bottom: 10px;
+    }
 
-            .item-i {
-                width: 50px;
-                height: 50px;
-            }
+    .nav-content {
+        display: flex;
+        flex-direction: column;
+    }
 
-            .item-text {
-                height: 75%;
-                display: flex;
-                justify-content: center;
-                align-items: end;
-                font-weight: bold;
-                color: #1e796a;
-            }
+    .nav-content-btn {
+        width: 60vw;
+        height: 10vh;
+        /* background-color: #bcd7d2; */
+        color: #1e796a;
+        font-weight: bold;
+        font-size: 24px;
+        border-bottom: solid 1px black;
+        display: flex;
+        align-items: end;
+        justify-content: center;
+    }
+</style>
 
-            .item {
-                width: 100%;
-                display: grid;
-                grid-template-columns: 1fr 2fr;
-                margin-top: 40px;
-            }
-
-            .bar {
-                width: 100%;
-                height: 1px;
-                background-color: black;
-            }
-        </style>
-    </head>
-
-    <body>
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="menuIcon"
-            onclick="showElement()"
-        >
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-        </svg>
-        <div class="menu" id="menu">
-            <div class="flex">
-                <div class="overlay" onclick="hideElement()"></div>
-                <div class="navbar">
-                    <div class="top">Menu</div>
-                    <div class="content">
-                        <a href="/dashboard">
-                            <div class="item">
-                                <div class="item-i">
-                                    <i class="fa-solid fa-house fa-2x"></i>
-                                </div>
-                                <div class="item-text">Home</div>
-                            </div>
-                        </a>
-                        <div class="bar"></div>
-
-                        <a href="{{ route('dashboard.manageProfile.index') }}">
-                            <div class="item">
-                                <div class="item-i">
-                                    <i class="fa-solid fa-user fa-2x"></i>
-                                </div>
-                                <div class="item-text">Profile</div>
-                            </div>
-                        </a>
-                        <div class="bar"></div>
-
-                        <a href="{{ route('dashboard.matches.index') }}">
-                            <div class="item">
-                                <div class="item-i">
-                                    <i class="fa-solid fa-heart fa-2x"></i>
-                                </div>
-                                <div class="item-text">Matches</div>
-                            </div>
-                        </a>
-                        <div class="bar"></div>
-
-                        <a href="/dashboard">
-                            <div class="item">
-                                <div class="item-i">
-                                    <i class="fa-solid fa-inbox fa-2x"></i>
-                                </div>
-                                <div class="item-text">Inbox</div>
-                            </div>
-                        </a>
-                        <div class="bar"></div>
-
-                        <a href="/">
-                            <div class="item">
-                                <div class="item-i">
-                                    <i class="fa-solid fa-phone fa-2x"></i>
-                                </div>
-                                <div class="item-text">Support</div>
-                            </div>
-                        </a>
-                        <div class="bar"></div>
-
-                        <a href="/dashboard/logout">
-                            <div class="item">
-                                <div class="item-i">
-                                    <i
-                                        class="fa-solid fa-right-from-bracket fa-2x"
-                                    ></i>
-                                </div>
-                                <div class="item-text">
-                                    {{ __("Log Out") }}
-                                </div>
-                            </div>
-                        </a>
-                        <div class="bar"></div>
-                    </div>
-                </div>
+<div class="menu" id="menu">
+    <div class="menu-content">
+        <div class="menu-content-overlay" onclick="closeMenu()"></div>
+        <div class="menu-content-nav">
+            <div class="nav-top">Menu</div>
+            <div class="nav-content">
+                <a href="/dashboard" class="nav-content-btn">
+                    <div></div>
+                    <div>Home</div>
+                </a>
+                <a
+                    href="{{ route('dashboard.manageProfile.index') }}"
+                    class="nav-content-btn"
+                >
+                    <div></div>
+                    <div>Profiel</div>
+                </a>
+                <a
+                    href="{{ route('dashboard.matches.index') }}"
+                    class="nav-content-btn"
+                >
+                    <div></div>
+                    <div>Matches</div>
+                </a>
+                <a href="/dashboard" class="nav-content-btn">
+                    <div></div>
+                    <div>Inbox</div>
+                </a>
+                <a href="/dashboard/support" class="nav-content-btn">
+                    <div></div>
+                    <div>Support</div>
+                </a>
+                <a href="/dashboard/logout" class="nav-content-btn">
+                    <div></div>
+                    <div>Log uit</div>
+                </a>
             </div>
         </div>
-    </body>
-    <!-- <script src="app.js"></script> -->
-    <script>
-        function showElement() {
-            document.getElementById("menu").style.display = "block";
-        }
-        function hideElement() {
-            document.getElementById("menu").style.display = "none";
-        }
-    </script>
-</html>
+    </div>
+</div>
+
+<script>
+    function openMenu() {
+        document.getElementById("menu").style.display = "block";
+    }
+    function closeMenu() {
+        document.getElementById("menu").style.display = "none";
+    }
+</script>
