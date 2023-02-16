0ad14d7db26d259db644f25a7886c9920c711edd
Gino Hanemaaijer <g.hanemaaijer11@gmail.com>
2023-02-16 13:20:18 +0100

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -75,7 +75,6 @@
             </svg>
         </div>
 
-        <x-jet-validation-errors class="mb-4" />
 
         @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
