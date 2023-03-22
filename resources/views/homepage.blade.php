0b61bce8e96028aefc88fd4f330c652b699fe699
ruben van beveren <rubenvanbeveren03@gmail.com>
2023-03-22 09:03:16 +0100

    hcjvhk

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -49,7 +49,7 @@
                 <!-- Logo -->
                 <div class=" mt-3">
                     <div class="flex justify-between w-screen">
-                        <?xml version="1.0" encoding="UTF-8"?><svg id="Laag_1" xmlns="http://www.w3.org/2000/svg"
+                        <svg id="Laag_1" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 559.14 263.07" class="w-36 ml-5">
                             <defs>
                                 <style>
