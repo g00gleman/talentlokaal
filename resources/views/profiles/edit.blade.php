6d299bcad6077405c0edcf0ef6c8f48b7f5e9aa5
Gino Hanemaaijer <g.hanemaaijer11@gmail.com>
2023-03-29 20:25:29 +0200

    changes

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -472,52 +472,56 @@
                                 {{ $message }}
                                 @enderror
                             </div>
-
                             <div
-                                class="col-span-5 flex justify-end text-center"
-                            >
-                            </div>
+                            class="col-span-5 flex justify-end text-center"
+                        >
+                        </div>
                             @endif
+                            
                             @if(isset($user->employer))
                                 {{-- companyname --}}
+                                <div class="col-span-1 mx-auto my-auto">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-10 h-10 mr-5">
+                                        stroke-width="1.5" fill="#000000FF" stroke="#000000FF" class="w-10 h-10 mr-5  ">
                                         <path stroke-linecap="round" stroke-linejoin="round"
-                                              d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
+                                        d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
                                     </svg>
+                                </div>
                                 <div
                                 class="col-span-4 text-xs flex items-center font-bold text-talent-orange"
                             >
 
 
                                     <input id="companyName"
-                                                 class="w-full focus:ring-talent-orange border-none rounded-md"
-                                                 type="text" name="bedrijfsnaam" value="{{ $user->employer->companyName }}"  autofocus
-                                                 placeholder="Vul hier uw bedrijfsnaam in" />
+                                                class="w-full focus:ring-talent-orange border-none rounded-md"
+                                                type="text" name="bedrijfsnaam" value="{{ $user->employer->companyName }}"  autofocus
+                                                placeholder="Vul hier uw bedrijfsnaam in" />
                                     @error('bedrijfsnaam')
                                     {{ $message }}
                                     @enderror
                                 </div>
                                 {{-- websiteurl --}}
-
+                                <div class="col-span-1 mx-auto my-auto">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-10 h-10 mr-5">
+                                        stroke-width="1.5" fill="#000000FF" stroke="#000000FF" class="w-10 h-10 mr-5">
                                         <path stroke-linecap="round" stroke-linejoin="round"
-                                              d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                                            d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
                                     </svg>
+                                </div>
                                     <div
                                     class="col-span-4 text-xs flex items-center font-bold text-talent-orange"
                                 >
                                     <input id="websiteUrl"
-                                                 class=" w-full focus:ring-talent-orange border-none rounded-md"
-                                                 type="text" name="websitelink" value="{{ $user->employer->websiteUrl }}"  autofocus
-                                                 placeholder="Link naar website" />
+                                                class=" w-full focus:ring-talent-orange border-none rounded-md"
+                                                type="text" name="websitelink" value="{{ $user->employer->websiteUrl }}"  autofocus
+                                                placeholder="Link naar website" />
                                         @error('websitelink')
                                         {{ $message }}
                                         @enderror
                                 </div>
                             @endif
                         </div>
+                    
                     </div><br><br>
       </div>
       </div>
