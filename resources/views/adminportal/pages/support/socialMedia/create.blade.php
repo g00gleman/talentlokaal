fc8a3dc8c777b371e78d2fa38cb2d532653035e9
DarkMrAnime <gyllian121@gmail.com>
2023-03-30 10:09:56 +0200

    a

--- resources/views/adminportal/pages/support/socialMedia/create.blade.php
+++ resources/views/adminportal/pages/support/socialMedia/create.blade.php
@@ -1,95 +1,102 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Nieuws</title>
+        @vite('resources/css/app.css')
+        <style>
+            html,
+            body {
+                margin: 0;
+                padding: 0;
+            }
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Nieuws</title>
-    @vite('resources/css/app.css')
-    <style>
-        html,
-        body {
-            margin: 0;
-            padding: 0;
-        }
+            body {
+                background-color: #1e796a;
+            }
 
-        body {
-            background-color: #1e796a;
-        }
+            .body-flex {
+                display: flex;
+            }
 
-        .body-flex {
-            display: flex;
-        }
+            .content {
+                padding-top: 25px;
+                width: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                overflow: scroll;
+            }
 
-        .content {
-            padding-top: 25px;
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            overflow: scroll;
-        }
+            .form {
+                background-color: white;
+                display: flex;
+                align-items: center;
+                min-height: 50vh;
+                padding-inline: 25px;
+                padding-block: 25px;
+                border-radius: 14px;
+                box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+            }
 
-        .form {
-            background-color: white;
-            display: flex;
-            align-items: center;
-            min-height: 50vh;
-            padding-inline: 25px;
-            padding-block: 25px;
-            border-radius: 14px;
-            box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-        }
+            .title {
+                border-radius: 111px;
+                border: 2px solid #1e796a;
+                margin-top: 10px;
+            }
 
-        .title {
-            border-radius: 111px;
-            border: 2px solid #1e796a;
-            margin-top: 10px;
-        }
+            .desc {
+                border-radius: 14px;
+                border: 2px solid #1e796a;
+                min-height: 100px;
+            }
 
-        .desc {
-            border-radius: 14px;
-            border: 2px solid #1e796a;
-            min-height: 100px;
-        }
+            .submit {
+                margin-top: 10px;
+                width: 125px;
+                height: 40px;
+                border-radius: 111px;
+                background-color: #f0850b;
+                color: white;
+                font-weight: bold;
+            }
 
-        .submit {
-            margin-top: 10px;
-            width: 125px;
-            height: 40px;
-            border-radius: 111px;
-            background-color: #f0850b;
-            color: white;
-            font-weight: bold;
-        }
+            .justifyEnd {
+                display: flex;
+                justify-content: end;
+            }
+        </style>
+    </head>
 
-        .justifyEnd {
-            display: flex;
-            justify-content: end;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="body-flex">
-        @include('adminportal.components.navbar.index')
-        <div class="content">
-            <div class="form">
-                <form action="/admin/support/create" method="post" class="formNews">
-                    @csrf
-                    <input class="title" type="text" name="mediaLink" id="mediaLink"
-                        placeholder="mediaLink" /><br />
-                    <div class="justifyEnd">
-                        <button type="submit" class="submit">
-                            Opslaan
-                        </button>
-                    </div>
-                </form>
+    <body>
+        <div class="body-flex">
+            @include('adminportal.components.navbar.index')
+            <div class="content">
+                <div class="form">
+                    <form
+                        action="/admin/support/create"
+                        method="post"
+                        class="formNews"
+                    >
+                        @csrf
+                        <input
+                            class="title"
+                            type="text"
+                            name="mediaLink"
+                            id="mediaLink"
+                            placeholder="mediaLink"
+                        /><br />
+                        <div class="justifyEnd">
+                            <button type="submit" class="submit">
+                                Opslaan
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-</body>
-
+    </body>
 </html>
